337b21b82f20634eb9f6664c31090b9989254215
Sarisha Das <sarishadas.810@gmail.com>
2022-08-16 19:25:16 -0700

    first1 again1

--- app.py
+++ app.py
@@ -8,16 +8,35 @@ Created on Tue Aug 16 06:26:15 2022
 
 import flask
 from flask import request
-from datetime import datetime as dt
+from datetime import datetime
 from dateutil import parser
+
+from sunpy.coordinates import sun
+import math
+
+def convert(x):
+    y=0
+    try:
+        y=parser.parse(x, fuzzy=True)
+    except:
+        if("now" in x or "today" in x):
+            y=datetime.now()
+        else:
+            print("date not understood")
+
+    if y:
+        return math.floor(sun.carrington_rotation_number(y))
+    else:
+        return 0
+
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     x = str(request.args['text'])
-    y=parser.parse(x, fuzzy=True)
+    cr=convert(x)
     try:
-        return "done"
+        return cr
     except KeyError:
         return 'bye'
