bb34606c0feb6f2fc8f4e3512156e16a4658b47a
Sarisha Das <sarishadas.810@gmail.com>
2022-08-21 18:48:50 -0700

    lxml test

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ from scipy.interpolate import interp1d
 
 import requests
 from bs4 import BeautifulSoup
+import lxml
 
 
 def convert(x):
@@ -59,7 +60,7 @@ r = requests.get(URL)
 cr = 2053           # Carrignton rotation number
 mag = 'GONG'        # Magnetogram type (GONG/HMI)
 
-soup = BeautifulSoup(r.content, 'html5lib')
+soup = BeautifulSoup(r.content, 'lxml')
 
 alla=soup.find_all("a", href=True)
 data = '' 
